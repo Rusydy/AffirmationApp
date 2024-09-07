6a8ef0173e1f5023ff29451004c452f1a9d9094f
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-09-08 02:54:16 +0700

    Create a list item card

--- app/src/main/java/com/example/affirmationapp/MainActivity.kt
+++ app/src/main/java/com/example/affirmationapp/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmationapp.data.Datasource
+import com.example.affirmationapp.model.Affirmation
 import com.example.affirmationapp.ui.theme.AffirmationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AffirmationAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AffirmationsApp() {
+    val affirmations = Datasource().loadAffirmations()
+    Column {
+        for (affirmation in affirmations) {
+            AffirmationCard(affirmation = affirmation)
+        }
+    }
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AffirmationCardPreview() {
+    AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AffirmationPreview() {
     AffirmationAppTheme {
-        Greeting("Android")
+        AffirmationsApp()
     }
 }
