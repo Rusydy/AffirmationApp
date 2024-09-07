cd2eee7f30c4ff8c6d1c5f54c3141ffa129e6d2a
Rusydy Muhiddin <rusydymuhiddin@gmail.com>
2024-09-08 03:33:51 +0700

    Add list of cards

--- app/src/main/java/com/example/affirmationapp/MainActivity.kt
+++ app/src/main/java/com/example/affirmationapp/MainActivity.kt
@@ -6,15 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,7 +39,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AffirmationAppTheme {
-
+                AffirmationsApp()
             }
         }
     }
@@ -37,11 +47,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationsApp() {
-    val affirmations = Datasource().loadAffirmations()
-    Column {
-        for (affirmation in affirmations) {
-            AffirmationCard(affirmation = affirmation)
-        }
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateEndPadding(layoutDirection)
+            )
+    ) {
+        val affirmations = Datasource().loadAffirmations()
+        AffirmationCardList(affirmations = affirmations)
     }
 }
 
@@ -66,10 +87,15 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview
 @Composable
-private fun AffirmationCardPreview() {
-    AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
+fun AffirmationCardList(affirmations: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmations) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation, modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
